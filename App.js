6f777198859a6f63a790ef05e918a4be97deb1df
aditya051312 <adityakhillari1@gmail.com>
2024-03-26 18:06:28 +0530

    Added App component for student attendance tracking application

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './Login';
+import Registration from './Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
