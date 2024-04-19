0a902e3850c117d18f010dd18db43964f886d2ce
aditya051312 <adityakhillari1@gmail.com>
2024-04-19 15:06:44 +0530

    Add test file for AddStudent component

---
+++ tests/AddStudent.test.js
@@ -0,0 +1,17 @@
+describe('AddStudent Component', () => {
+  beforeEach(() => {
+    cy.visit('/add-student')
+  })
+
+  it('should render the Add Student form', () => {
+    // Test case implementation
+  })
+
+  it('should add a new student when the form is submitted', () => {
+    // Test case implementation
+  })
+
+  it('should close the modal when the Cancel button is clicked', () => {
+    // Test case implementation
+  })
+})
