666e6e5eaa099485bd7e94eb641b316d52e04509
aditya051312 <adityakhillari1@gmail.com>
2024-04-19 15:18:09 +0530

    Add test cases for AddStudent component

---
+++ AddStudent.spec.js
@@ -0,0 +1,11 @@
+describe('Add Student', () => {
+  it('should add a new student', () => {
+    // Test case code here
+  });
+
+  it('should navigate to the dashboard after adding a student', () => {
+    // Test case code here
+  });
+
+  // More test cases...
+});
